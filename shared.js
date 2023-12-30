bc8fe5c349a6e20d91cf6496279f4d4fb15453a5
Zarina <tochilka007@mail.ru>
2023-12-30 19:07:18 +0300

    Гребаная бегущая строка.

--- shared.js
+++ shared.js
@@ -181,6 +181,21 @@ new Swiper('.image-slider', {
 } );
 
 
+$(function() {
+	var marquee = $("#marquee"); 
+	marquee.css({"overflow": "hidden", "width": "200%"});
+	// оболочка для текста ввиде span (IE не любит дивы с inline-block)
+	marquee.wrapInner("<span>");
+	marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+	marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
+	marquee.wrapInner("<div>");
+	marquee.find("div").css("width", "200%");
+	var reset = function() {
+		$(this).css("margin-left", "0%");
+		$(this).animate({ "margin-left": "-100%" }, 14000, 'linear', reset);
+	};
+	reset.call(marquee.find("div"));
+});
 
 
 
